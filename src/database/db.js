a95968b81fe9d6c895c530ccc02efa4e54a3e31d
Lucas Seguessi <lucas.seguessi@gmail.com>
2020-06-06 23:27:05 -0300

    Create db.js

--- src/database/db.js
+++ src/database/db.js
@@ -5,4 +5,39 @@ const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database('./src/database/database.db')
 
 // Utilizar o objeto de banco de dados para nossas operações
-db.serialize()
+db.serialize(() => {
+    // com comandos SQL
+    
+    // 1-Criar uma tabela
+    db.run(`
+        CREATE TABLE IF NOT EXIST places (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            image TEXT, 
+            address TEXT, 
+            address2 TEXT, 
+            state TEXT, 
+            city TEXT, 
+            items TEXT
+        );
+    `)
+
+    // 2-Inserir dados na tabela
+    const query = `
+        INSERT INTO places (
+            name, 
+            image, 
+            address, 
+            address2, 
+            state, 
+            ity, 
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+    `
+    db.run(query)
+
+    // 3-Consultar dados da tabela
+
+    // 4-Deletar um dado da tabela
+
+})
